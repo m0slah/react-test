2ac069e7e229dc2aa9d2dbfdc1ca0efb7e74a462
m0slah <muhammedslahqadir@gmail.com>
2024-07-24 11:38:47 +0300

    add name state and pass to ComponentB

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import ComponentB from "./ComponentB";
 
 const ComponentA = () => {
+  const [name, setname] = useState("dave");
+
   return (
     <div>
       <div className="box">
         <p>Component A</p>
-        <ComponentB />
+        <p>hello: {name}</p>
+        <ComponentB name={name} />
       </div>
     </div>
   );
